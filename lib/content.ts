80fcc9d678ab50472782e7f56b09e69b45037698
Gabriel H. Dalmoro <ghdalmoro@gmail.com>
2025-02-11 15:45:12 -0400

    add website content text; format home page

---
+++ lib/content.ts
@@ -0,0 +1,42 @@
+export const textContent = {
+  english: {
+    bio: "An innovative and adaptable developer, I blend a passion for tech advancement with a creative vision for design and collaborative excellence.",
+    about:
+      "I am a dedicated developer with a passion for continuous learning and a creative eye for design, driving innovation in technology. My adaptability and commitment to meaningful collaboration contribute to my pursuit of crafting impactful solutions and forming strong professional relationships.",
+    projects: [
+      {
+        title: "Ski Boot App",
+        description:
+          "Developed a robust web application for custom ski boot fitting using 3D foot scanning technology. Engineered a user-friendly profile interface and managed the backend to support thousands of customers. Designed an innovative boot card feature showcasing 3D scanned feet using Three.js visualization.",
+        technologies: "Python, Django, PostgreSQL, Three.js",
+        skills: "",
+        id: 0,
+      },
+      {
+        title: "",
+        description: "",
+        technologies: "",
+        skills: "",
+        id: 1,
+      },
+      {
+        title: "",
+        description: "",
+        technologies: "",
+        skills: "",
+        id: 2,
+      },
+    ],
+    // technologies: [],
+  },
+  portuguese: {
+    bio: "Esse é a minha bio",
+    // projects: {},
+    // technologies: [],
+  },
+  spanish: {
+    bio: "Eso es mi bio",
+    // projects: {},
+    // technologies: [],
+  },
+};
